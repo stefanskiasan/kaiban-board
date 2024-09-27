be7eba21e46ec9640e79a79097fd6eea469f3508
OdinSweng <odin.sweng@gmail.com>
2024-09-26 21:31:32 -0400

    Refactor AgentAvatar component to update styling and add ring

--- src/components/KaibanBoard/components/Common/AgentAvatar/index.jsx
+++ src/components/KaibanBoard/components/Common/AgentAvatar/index.jsx
@@ -3,8 +3,8 @@ const AgentAvatar = ({ agent, size }) => {
     const avatarUrl = `https://robohash.org/${agent?.name}`;
 
     return (
-        <div className="bg-slate-950/50 rounded-full">
-            <img className={`rounded-full ${size === "xl" ? "w-11 h-11" : size === "lg" ? "w-9 h-9" : size === "md" ? "w-8 h-8" : "w-7 h-7"}`}
+        <div className="bg-slate-800 rounded-full ring-2 ring-slate-700">
+            <img className={`rounded-full bg-slate-950/50 ${size === "xl" ? "w-11 h-11" : size === "lg" ? "w-9 h-9" : size === "md" ? "w-8 h-8" : "w-7 h-7"}`}
                 src={avatarUrl} alt="agent"/>
         </div>
     );
